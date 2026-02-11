51a94d87ea35bdcbc968648061e32f146bf1716e
1abdulsattar32304-prog <1abdulsattar32304@gmail.com>
2026-02-11 14:32:39 +0500

    Factoral and other function add here

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("Hello, Three!")
 a = 20
 b = 34
 print("Sum:", a + b)
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("Alice"))
+def factorial(n):
+        if n == 0:
+         return 1
+        else:
+         return n * factorial(n - 1) 
